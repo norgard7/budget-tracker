f2106492dcb23bbccfb91d4175bf07d73138c4d7
Lauren Norgard <lauren.c.schumann@gmail.com>
2021-08-22 21:44:36 -0500

    in server.js, trying to fix a port bug.

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,11 +16,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
@@ -29,6 +25,12 @@ mongoose.connect(
     useFindAndModify: false
   }
 );
+
+
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 
 // routes
